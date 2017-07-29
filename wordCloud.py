abc48aa828839a30f6d56d63fef498c495909b75
DESKTOP-DLOBL4I\张文喆 <942740938@qq.com>
2017-07-29 19:43:56 +0800

    添加了词频统计函数

--- wordCloud.py
+++ wordCloud.py
@@ -4,6 +4,19 @@ import jieba
 import commentSpider
 
 
+def countFreq(_wordList):
+    word_dict = {}
+    for word in _wordList:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+
+    ordered_list = sorted(word_dict.items(), key=lambda item: item[1], reverse=True)
+    print(ordered_list)
+    return word_dict  # 返回词频字典
+
+
 def makeCloud(_commentList):
     word_list = []
     for comment in _commentList:
@@ -11,7 +24,9 @@ def makeCloud(_commentList):
 
     word_split = ' '.join(word_list)
     print(word_split)
-    comment_cloud = wordcloud.WordCloud(font_path='DroidSansFallback.ttf', max_words=100).generate(word_split)
+    # ordered_list = countFreq(word_list)
+    # comment_cloud = wordcloud.WordCloud(font_path='Deng.ttf', max_words=100).fit_words(ordered_list)
+    comment_cloud = wordcloud.WordCloud(font_path='DroidSansFallback.ttf', max_words=50, max_font_size=50).generate(word_split)
 
     plt.imshow(comment_cloud)
     plt.axis('off')  # 关闭刻度
